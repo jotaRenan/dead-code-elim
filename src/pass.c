b2352a8d61eabcb465fc2e5cbc37c44b77ddfc12
João Pedro Renan <jota.renan@gmail.com>
2021-07-10 11:28:08 -0300

    Create pass.c

---
+++ src/pass.c
@@ -0,0 +1,15 @@
+bool::RADeadCodeElimination::solveICmpInstruction(ICmpInst* I) {
+  InterProceduralRA < Cousot >* ra = &getAnalysis < InterProceduralRA < Cousot > >();
+  Range range1 = ra->getRange(I->getOperand(0));
+  Range range2 = ra->getRange(I->getOperand(1));
+  switch (I->getPredicate()){
+    case CmpInst::ICMP_SLT:
+    //This code is always true
+    if (range1.getUpper().slt(range2.getLower())) {
+      ...
+    }
+    break;
+    case ...
+  }
+  ...
+}
