1e1920cec763e323dba897d6e6df0a11fe699815
Paula Ribeiro <paula.mribeiro05@gmail.com>
2021-07-24 17:25:47 -0300

    cast iterator to basic block

--- src/vSSA.cpp
+++ src/vSSA.cpp
@@ -41,7 +41,7 @@ bool vSSA::runOnFunction(Function &F) {
   // Iterate over all Basic Blocks of the Function, calling the function that
   // creates sigma functions, if needed
   for (Function::iterator Fit = F.begin(), Fend = F.end(); Fit != Fend; ++Fit) {
-    createSigmasIfNeeded(Fit);
+    createSigmasIfNeeded(dyn_cast<BasicBlock>(Fit));
   }
   return true;
 }
